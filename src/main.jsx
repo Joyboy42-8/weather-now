67027ba290be132e9430b20808d63ef35ff4882a
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-11-02 16:14:13 +0000

    Deleting genai import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
-import { TurnCoverage } from '@google/genai'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
